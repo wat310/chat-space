ad3af2e3430cc42e1f372912196b21daeac994ab
wat310 <midtilda_velca@yahoo.co.jp>
2019-08-19 20:05:32 +0900

    comment

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,14 @@
 class Group < ApplicationRecord
   has_many :user_groups
   has_many :users, through: :user_groups
+  has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   has_many :messages
   has_many :user_groups
+  has_many :messages
   has_many :groups, through: :user_groups
 end
