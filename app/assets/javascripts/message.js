8571f9e188229bf1c2e9a230a5fdb3776478964c
wat310 <midtilda_velca@yahoo.co.jp>
2019-08-23 19:01:35 +0900

    スクロール、連続ボタン(まだデータの受け渡しに難あり)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,29 @@
 $(function(){
+
+  function buildHTML(message){
+    var html = `<div class="chat-box">
+                  <div class="info-box">
+                    <div class="info-box__name">
+                      ${message.user_name}
+                    </div>
+                    <div class="info-box__date">
+                      ${message.time}
+                    </div>
+                  </div>
+                  <div class="text-box">
+                    <p class="text-box__content">
+                      ${message.content}
+                    </p>
+                      ${message.image}
+                  </div>
+                </div>`
+    return html;
+  }
+
+  function scroll() { /*最下部までスクロールするメソッド*/
+    $('.main-chat').animate({scrollTop: $('.main-chat')[0].scrollHeight});
+}
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -12,10 +37,14 @@ $(function(){
       contentType: false
     })
     .done(function(message){
-      
+      var html = buildHTML(message);
+      $('.main-chat').append(html);
+      $('#message_content').val('');
+      $("input[ type='submit' ]").removeAttr('disabled'); /*連続でボタンを押せるように*/
+      scroll() /*scrollメソッドの呼び出し*/
     })
     .fail(function(){
-
+      alert('エラーです');
     })
   })
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,3 +1,4 @@
+json.user_name @message.user.name
 json.content @message.content
 json.image @message.image
 json.time @message.created_at.strftime("%Y/%m/%d %H:%M")
