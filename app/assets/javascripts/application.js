f3ddd27becba99e5cab9b88100a16fcb71a1ccd5
wat310 <midtilda_velca@yahoo.co.jp>
2019-08-23 16:27:17 +0900

    js.file create

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require rails-ujs
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier #8/23コメントアウト、デプロイする際のエラー回避
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
