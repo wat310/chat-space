7082cc79512680330240879e1dcf948bf31a4009
wat310 <midtilda_velca@yahoo.co.jp>
2019-08-25 20:09:22 +0900

    current_userを検索から削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    users_include_current_user = User.where('name LIKE(?)', "%#{params[:keyword]}%") #current_userも含んだ配列
+    @users = []
+    users_include_current_user.each do |user|
+      if user.id != current_user.id
+        @users << user #current_userを除いた配列の中で検索が行われるように
+      end
+    end
     respond_to do |format|
       format.html
       format.json
