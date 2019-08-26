6845f1c49c6ddde2711ba4f3b250edd9947ec478
wat310 <midtilda_velca@yahoo.co.jp>
2019-08-26 15:39:44 +0900

    setIntervalまで

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,26 +1,26 @@
 $(document).on('turbolinks:load', function() { /*turbolinksの影響で、グループ変更後１回目の投稿で非同期通信が働かない現象を解決*/
   $(function(){
     function buildHTML(message){
-      var image = (message.image) ? `<img src="${message.image}">` : ""; /*画像の有無の条件分岐*/
-
-      var html = `<div class="chat-box">
-                    <div class="info-box">
-                      <div class="info-box__name">
-                        ${message.user_name}
+      var image = (message.image.url) ? `<img src="${message.image.url}">` : ""; /*画像の有無の条件分岐*/
+        /*data-idが入るように*/
+        var html = `<div class="chat-box" data-id="chat">
+                      <div class="info-box">
+                        <div class="info-box__name">
+                          ${message.user_name}
+                        </div>
+                        <div class="info-box__date">
+                          ${message.time}
+                        </div>
                       </div>
-                      <div class="info-box__date">
-                        ${message.time}
+                      <div class="text-box">
+                        <p class="text-box__content">
+                          ${message.content}
+                        </p>
+                          ${image}
                       </div>
-                    </div>
-                    <div class="text-box">
-                      <p class="text-box__content">
-                        ${message.content}
-                      </p>
-                        ${image}
-                    </div>
-                  </div>`
+                    </div>`
       return html;
-    }
+    };
 
     function scroll() { /*最下部までスクロールするメソッド*/
       $('.main-chat').animate({scrollTop: $('.main-chat')[0].scrollHeight}, 'fast');
@@ -53,5 +53,28 @@ $(document).on('turbolinks:load', function() { /*turbolinksの影響で、グル
         $("input[ type='submit' ]").removeAttr('disabled'); /*連続でボタンを押せるように*/
       })
     })
+
+    var reloadMessages = function() { /*自動更新*/
+      last_message_id = $(".chat-box").data('chat') /*カスタムデータを取得*/
+      $.ajax({ /*リクエスト*/
+        url: 'api/messages', /*リクエストのパス(動かしたいコントローラーのアクションのルーティング)*/
+        type: 'get', /*/groups/:group_id/api/messagesのhttpメソッドはGET*/
+        dataType: 'json', /*json形式で送る*/
+        data: {id: last_message_id} /*リクエストで一緒に送るデータ*/
+      })
+      /*1*/
+      .done(function(messages) {
+        /*4*/
+        var insertHTML = '';
+        messages.forEach(function(message){
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat').append(insertHTML);
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    };
+    setInterval(reloadMessages, 5000) /*5000ミリ秒間隔で関数を呼び出す*/
   });
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,4 @@
+json.(@message, :content, :image) #json.content @message.content、 json.image @message.image(※ image.urlではない!)と意味は同じ(このように省略できる)
 json.user_name @message.user.name
-json.content @message.content
-json.image @message.image.url
 json.time @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.id @message.id
