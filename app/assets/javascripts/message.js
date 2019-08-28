ab7145e24161e6606a6aeda7553ca0a8fd25c5be
wat310 <midtilda_velca@yahoo.co.jp>
2019-08-28 13:49:04 +0900

    自動更新完成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,13 +3,13 @@ $(document).on('turbolinks:load', function() { /*turbolinksの影響で、グル
     function buildHTML(message){
       var image = (message.image.url) ? `<img src="${message.image.url}">` : ""; /*画像の有無の条件分岐*/
         /*data-idが入るように*/
-        var html = `<div class="chat-box" data-id="chat">
+        var html = `<div class="chat-box" data-message-id="${message.id}">
                       <div class="info-box">
                         <div class="info-box__name">
                           ${message.user_name}
                         </div>
                         <div class="info-box__date">
-                          ${message.time}
+                          ${message.created_at}
                         </div>
                       </div>
                       <div class="text-box">
@@ -55,25 +55,31 @@ $(document).on('turbolinks:load', function() { /*turbolinksの影響で、グル
     })
 
     var reloadMessages = function() { /*自動更新*/
-      last_message_id = $(".chat-box").data('chat') /*カスタムデータを取得*/
-      $.ajax({ /*リクエスト*/
-        url: 'api/messages', /*リクエストのパス(動かしたいコントローラーのアクションのルーティング)*/
-        type: 'get', /*/groups/:group_id/api/messagesのhttpメソッドはGET*/
-        dataType: 'json', /*json形式で送る*/
-        data: {id: last_message_id} /*リクエストで一緒に送るデータ*/
-      })
-      /*1*/
-      .done(function(messages) {
-        /*4*/
-        var insertHTML = '';
-        messages.forEach(function(message){
-          insertHTML += buildHTML(message)
+      if (window.location.href.match(/\/groups\/\d+\/messages/)) { /*グループのメッセージ一覧ページの時のみ自動更新を実行*/
+        // var last_message_id = $(".chat-box").last().data("message-id"); /*最後のカスタムデータを取得*/
+        var last_message_id = $(".chat-box:last").data("message-id"); 
+        $.ajax({ /*リクエスト*/
+          url: 'api/messages', /*リクエストのパス(動かしたいコントローラーのアクションのルーティング)*/
+          type: 'GET', /*/groups/:group_id/api/messagesのhttpメソッドはGET*/
+          dataType: 'json', /*json形式で送る*/
+          data: {id: last_message_id} /*リクエストで一緒に送るデータ*/
+        })
+        .done(function(messages) {
+          console.log("OK"); /*あとで消す！！*/
+          var insertHTML = '';
+          messages.forEach(function(message){
+            if (message.id > last_message_id) { /*前回から投稿が増えれば自動更新する*/
+              insertHTML += buildHTML(message)
+              scroll() /*scrollメソッドの呼び出し*/
+            }
+          });
+          $('.main-chat').append(insertHTML);
+        })
+        .fail(function() {
+          alert('非同期通信に失敗しました')
         });
-        $('.main-chat').append(insertHTML);
-      })
-      .fail(function() {
-        console.log('error');
-      });
+      }
+
     };
     setInterval(reloadMessages, 5000) /*5000ミリ秒間隔で関数を呼び出す*/
   });
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -3,6 +3,9 @@ class Api::MessagesController < ApplicationController
     group = Group.find(params[:group_id]) #ルーティングでの設定によりparamsの中にgroup_idというキーでグループのidが入るので、これを元にDBからグループを取得する
     last_message_id = params[:id].to_i #ajaxで送られてくる最後のメッセージのid番号を変数に代入
     @messages = group.messages.includes(:user).where("id > #{last_message_id}") #取得したグループのメッセージたちから、idがlast_message_idよりも新しいメッセージ達のみを取得
-    #2
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 end
